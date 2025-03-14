1c78d9271e5b98b55bbed2f67ea5ccff0eef416b
Abdulkhalek-1 <abdulkhalek.muhammad.work@gmail.com>
2025-03-14 13:59:23 +0200

    feat(users): enhance user model with bio and profile picture fields

--- backend/student_eye/users/models.py
+++ backend/student_eye/users/models.py
@@ -2,8 +2,7 @@
 from typing import ClassVar
 
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.db.models import EmailField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
@@ -18,11 +17,22 @@ class User(AbstractUser):
     """
 
     # First and last name do not cover name patterns around the globe
-    name = CharField(_("Name of User"), blank=True, max_length=255)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
     first_name = None  # type: ignore[assignment]
     last_name = None  # type: ignore[assignment]
-    email = EmailField(_("email address"), unique=True)
+    email = models.EmailField(_("email address"), unique=True)
     username = None  # type: ignore[assignment]
+    bio = models.TextField(
+        _("Bio"),
+        blank=True,
+        default="",
+    )
+    picture = models.ImageField(
+        _("Profile Picture"),
+        upload_to="profile_pictures",
+        blank=True,
+        null=True,
+    )
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
